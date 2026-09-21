b6307c6397a9ee8b0defa1ebb26cc80a4d27391c
Don Barrigon <soydonbarrigon@gmail.com>
2026-09-21 14:54:19 -0500

    feat: mongo model

--- core/src/db/mongo_model.rs
+++ core/src/db/mongo_model.rs
@@ -1,4 +1,7 @@
-use mongodb::bson::{Document, oid::ObjectId};
+use mongodb::{
+    Collection,
+    bson::{Document, oid::ObjectId},
+};
 use serde::{Serialize, de::DeserializeOwned};
 
 use crate::{
@@ -17,6 +20,10 @@ pub trait ODModel: Serialize + DeserializeOwned + Send + Sync + Clone + 'static 
         let id = ODMongo::hex_to_object_id(hex_id)?;
         self.set_id(id);
         Ok(())
+    }
+
+    fn coll() -> Result<Collection<Self>, ForgeError> {
+        Ok(odm()?.coll::<Self>(Self::coll_name()))
     }
 
     // == Hooks ===================================================
@@ -46,60 +53,136 @@ pub trait ODModel: Serialize + DeserializeOwned + Send + Sync + Clone + 'static 
         Ok(())
     }
 
+    // == Many Operations =========================================
+
+    fn create_many(docs: &mut Vec<Self>) -> impl Future<Output = Result<(), ForgeError>> + Send {
+        async move { odm()?.create_many::<Self>(docs).await }
+    }
+
+    fn update_many(
+        filter: Document,
+        update: Document,
+    ) -> impl Future<Output = Result<mongodb::results::UpdateResult, ForgeError>> + Send {
+        async move { odm()?.update_many::<Self>(filter, update).await }
+    }
+
     // == CRUD ====================================================
 
     fn create(&mut self) -> impl Future<Output = Result<(), ForgeError>> + Send {
-        async move { odm()?.create(self).await }
+        async move { odm()?.create::<Self>(self).await }
     }
 
     fn update(&mut self) -> impl Future<Output = Result<(), ForgeError>> + Send {
-        async move { odm()?.update(self).await }
+        async move { odm()?.update::<Self>(self).await }
     }
 
     fn save(&mut self) -> impl Future<Output = Result<(), ForgeError>> + Send {
-        async move { odm()?.save(self).await }
+        async move { odm()?.save::<Self>(self).await }
     }
 
     fn replace(&mut self) -> impl Future<Output = Result<(), ForgeError>> + Send {
-        async move { odm()?.replace(self).await }
+        async move { odm()?.replace::<Self>(self).await }
     }
 
     fn delete(&mut self) -> impl Future<Output = Result<(), ForgeError>> + Send {
-        async move { odm()?.delete(self).await }
+        async move { odm()?.delete::<Self>(self).await }
     }
 
     // == Getting previus =========================================
 
     fn update_getting_previous(&mut self) -> impl Future<Output = Result<Self, ForgeError>> + Send {
-        async move { odm()?.update_getting_previous(self).await }
+        async move { odm()?.update_getting_previous::<Self>(self).await }
     }
 
     fn replace_getting_previous(&mut self) -> impl Future<Output = Result<Self, ForgeError>> + Send {
-        async move { odm()?.replace_getting_previous(self).await }
+        async move { odm()?.replace_getting_previous::<Self>(self).await }
     }
 
     fn delete_getting_previous(id: ObjectId) -> impl Future<Output = Result<Self, ForgeError>> + Send {
-        async move { odm()?.delete_getting_previous(id).await }
+        async move { odm()?.delete_getting_previous::<Self>(id).await }
     }
 
     // == Getters =================================================
 
     fn get_by_id(id: ObjectId) -> impl Future<Output = Result<Self, ForgeError>> + Send {
-        async move { odm()?.get_by_id(id).await }
+        async move { odm()?.get_by_id::<Self>(id).await }
     }
 
     fn get_by_hex_id(hex_id: &str) -> impl Future<Output = Result<Self, ForgeError>> + Send {
-        async move { odm()?.get_by_hex_id(hex_id).await }
+        async move { odm()?.get_by_hex_id::<Self>(hex_id).await }
     }
 
     fn get_all() -> impl Future<Output = Result<Vec<Self>, ForgeError>> + Send {
-        async move { odm()?.get_all().await }
+        async move { odm()?.get_all::<Self>().await }
     }
 
     fn find<U>(filter: Document) -> Result<ODMFind<Self, U>, ForgeError>
     where
         U: Serialize + DeserializeOwned + Send + Sync + 'static,
     {
-        Ok(odm()?.find(filter))
+        Ok(odm()?.find::<Self, U>(filter))
+    }
+}
+
+pub trait ODModelCollection<T: ODModel> {
+    fn create(&mut self) -> impl Future<Output = Result<(), ForgeError>> + Send;
+    fn update(&mut self) -> impl Future<Output = Result<(), ForgeError>> + Send;
+    fn save(&mut self) -> impl Future<Output = Result<(), ForgeError>> + Send;
+    fn replace(&mut self) -> impl Future<Output = Result<(), ForgeError>> + Send;
+    fn delete(&mut self) -> impl Future<Output = Result<(), ForgeError>> + Send;
+}
+
+impl<T: ODModel> ODModelCollection<T> for Vec<T> {
+    fn create(&mut self) -> impl Future<Output = Result<(), ForgeError>> + Send {
+        async move {
+            for m in self.iter_mut() {
+                m.before_create()?;
+            }
+            odm()?.create_many(self).await?;
+            for m in self.iter_mut() {
+                m.after_create()?;
+            }
+            Ok(())
+        }
+    }
+
+    fn update(&mut self) -> impl Future<Output = Result<(), ForgeError>> + Send {
+        async move {
+            // hacerlo con bulkWrite
+            for m in self.iter_mut() {
+                m.update().await?;
+            }
+            Ok(())
+        }
+    }
+
+    // one to one
+    fn save(&mut self) -> impl Future<Output = Result<(), ForgeError>> + Send {
+        async move {
+            for m in self.iter_mut() {
+                m.save().await?;
+            }
+            Ok(())
+        }
+    }
+
+    // one to one
+    fn replace(&mut self) -> impl Future<Output = Result<(), ForgeError>> + Send {
+        async move {
+            for m in self.iter_mut() {
+                m.replace().await?;
+            }
+            Ok(())
+        }
+    }
+
+    // one to one
+    fn delete(&mut self) -> impl Future<Output = Result<(), ForgeError>> + Send {
+        async move {
+            for m in self.iter_mut() {
+                m.delete().await?;
+            }
+            Ok(())
+        }
     }
 }
